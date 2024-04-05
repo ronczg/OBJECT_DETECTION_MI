33e7486b6500d8776ebca9ba362bd916af39c669
ronczg <roncz.gabor1@gmail.com>
2024-04-05 23:29:48 +0200

    ecsetvastagsággal trükközés

--- handtrack/VirtualPainter.py
+++ handtrack/VirtualPainter.py
@@ -33,26 +33,28 @@ imgCanvas = np.zeros((720,1280,3),np.uint8)
 while True:
     success, img = cap.read()
     img = cv2.flip(img,1)
-    
+
     img = detector.findHands(img)
     lmList = detector.findPosition(img, draw=False)
-    
+
     if len(lmList) != 0:
-       
+
         #print(lmList)
-        
-        
+
+
         x1,y1 = lmList[8][1:]
         x2,y2 = lmList[12][1:]
-        
+        x4,y4 = lmList[20][1:]
         fingers = detector.fingersUp()
         print(fingers)
-        
+
         if fingers[1] and fingers[2]:
             xp,yp = 0,0
             print("Selection Mode")
             if y1 < 125:
-                if 250 <x1<450:
+                if 0<x1<200:
+                    brushThickness += 20
+                elif 250 <x1<450:
                     header = overlayList[0]
                     drawColor = (0,0,255)
                 elif 650 <x1<750:
@@ -65,30 +67,41 @@ while True:
                     header = overlayList[3]
                     drawColor=(0,0,0)
             cv2.rectangle(img,(x1,y1-25),(x2,y2+25),drawColor,cv2.FILLED)
-            
+        # if fingers[1] and fingers[4]:
+        #         cv2.rectangle(img,(x1,y4-25),(x4,y4+25),drawColor,cv2.FILLED)
+        #         print("Drawing Mode")
+        #         if xp == 0 and yp == 0:
+        #             xp,yp = x1,y1
+        #         if drawColor == (0,0,0):
+        #             cv2.line(img, (xp,yp),(x1,y1),drawColor,ereaserThickness)
+        #             cv2.line(imgCanvas, (xp,yp),(x1,y1),drawColor,ereaserThickness)
+        #         else:
+        #             cv2.rectangle(img, (xp,yp),(x4,y4),drawColor,brushThickness)
+        #             cv2.rectangle(imgCanvas, (xp,yp),(x4,y4),drawColor,brushThickness)
+        #         xp,yp = x1,y1
         if fingers[1] and fingers[2]==False:
-            
+
             cv2.circle(img,(x1,y1),15,drawColor,cv2.FILLED)
             print("Drawing Mode")
             if xp == 0 and yp == 0:
                 xp,yp = x1,y1
             if drawColor == (0,0,0):
                 cv2.line(img, (xp,yp),(x1,y1),drawColor,ereaserThickness)
-                cv2.line(imgCanvas, (xp,yp),(x1,y1),drawColor,ereaserThickness)    
+                cv2.line(imgCanvas, (xp,yp),(x1,y1),drawColor,ereaserThickness)
             else:
                 cv2.line(img, (xp,yp),(x1,y1),drawColor,brushThickness)
                 cv2.line(imgCanvas, (xp,yp),(x1,y1),drawColor,brushThickness)
             xp,yp = x1,y1
-    
+
     imgGray = cv2.cvtColor(imgCanvas,cv2.COLOR_BGR2GRAY)
     _,imgInv = cv2.threshold(imgGray,50,255,cv2.THRESH_BINARY_INV)
     imgInv = cv2.cvtColor(imgInv,cv2.COLOR_GRAY2BGR)
     img = cv2.bitwise_and(img,imgInv)
     img = cv2.bitwise_or(img,imgCanvas)
-    
-        
+
+
     img[0:125,0:1280] = header
-    
+
     cv2.imshow("Image",img)
-    cv2.imshow("Canvas",imgCanvas)
+    #cv2.imshow("Canvas",imgCanvas)
     cv2.waitKey(1)
