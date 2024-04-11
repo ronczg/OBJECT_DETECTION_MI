04cf00ce7f480ec580f653ce7770bb87bf18f412
ronczg <roncz.gabor1@gmail.com>
2024-04-11 15:21:37 +0200

    fingercounter kész

--- handtrack/Fingercounter.py
+++ handtrack/Fingercounter.py
@@ -11,12 +11,32 @@ cap.set(4,hCam)
 
 detector = htm.handDetector(detectionCon=0.75)
 
+tipIds = [4,8,12,16,20]
+
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
     lmList = detector.findPosition(img)
-    print(lmList)
-
+    #print(lmList)
+    if len(lmList) != 0:
+        fingers = []
+        #hüvelykujj
+        if lmList[tipIds[0]][1] > lmList[tipIds[0]-1][1]:
+                fingers.append(1)
+        else:
+                fingers.append(0)
+        #többiujj
+        for id in range(1,5):
+            if lmList[tipIds[id]][2] < lmList[tipIds[id]-2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        #print(fingers)
+        totalFingers = fingers.count(1)
+        print(totalFingers)
+        
+        cv2.rectangle(img,(20,225),(170,425),(0,255,50),cv2.FILLED)
+        cv2.putText(img,str(totalFingers),(45,375),cv2.FONT_HERSHEY_PLAIN,10,(255,0,0),20)
     
     cv2.imshow("Image",img)
     cv2.waitKey(1)
